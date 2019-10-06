390e9cdf71d4fe3dc34187beae3d5bc8cc91e063
Ambler <allinlbj@gmail.com>
2019-10-06 18:02:11 +0800

    链接携带数据

--- pages/lifecyle/lifecyle.js
+++ pages/lifecyle/lifecyle.js
@@ -5,14 +5,54 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    movies: [{
+      name: "肖申克的救赎",
+      img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p480747492.webp",
+      desc: "有的人的羽翼是如此光辉，即使世界上最黑暗的牢狱，也无法长久地将他围困！"
+    },
+    {
+      name: "霸王别姬",
+      img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p1910813120.webp",
+      desc: "风华绝代。"
+    },
+    {
+      name: "这个杀手不太冷",
+      img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p511118051.webp",
+      desc: "怪蜀黍和小萝莉不得不说的故事。"
+    },
+    {
+      name: "阿甘正传",
+      img: "https://img1.doubanio.com/view/photo/s_ratio_poster/public/p510876377.webp",
+      desc: "一部美国近现代史。"
+    },
+    {
+      name: "美丽人生",
+      img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p510861873.webp",
+      desc: "最美的谎言。"
+    },
+    {
+      name: "泰坦尼克号",
+      img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p457760035.webp",
+      desc: "失去的才是永恒的。"
+    },
+    {
+      name: "千与千寻",
+      img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p1606727862.webp",
+      desc: "最好的宫崎骏，最好的久石让。"
+    },
+    {
+      name: "辛德勒名单",
+      img: "https://img3.doubanio.com/view/photo/s_ratio_poster/public/p492406163.webp",
+      desc: "拯救一个人，就是拯救整个世界。"
+    },
+    ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(options)
   },
 
   /**
